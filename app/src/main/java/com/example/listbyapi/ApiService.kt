9ebad5932fe87020436e19a0e9e433ae8a29a433
raed-charrad <raed.charrad91@gmail.com>
2022-11-10 15:45:54 +0100

    add delete and add offers funs

--- app/src/main/java/com/example/listbyapi/ApiService.kt
+++ app/src/main/java/com/example/listbyapi/ApiService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ApiService {
@@ -14,5 +15,7 @@ interface ApiService {
     suspend fun deleteOffer(@Path("id") id : Int)
     @POST("/Offres")
     suspend fun addOffer(@Body offer : Offre)
+    @PUT("/Offres/{id}")
+    suspend fun updateOffer(@Path("id") id : Int, @Body offer : Offre)
 
 }
